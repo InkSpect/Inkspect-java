4a72ab78078c955eb1587a865302ff868a5162d9
0xdbe <23268141+0xdbe@users.noreply.github.com>
2025-08-01 22:17:20 +0200

    fix: create AnalyzeSourceCode UseCase

--- src/main/scala/com/github/inkspect/java_cli/Main.scala
+++ src/main/scala/com/github/inkspect/java_cli/Main.scala
@@ -1,64 +1,16 @@
 package com.github.inkspect.java_cli
 
-import io.joern.javasrc2cpg.{Config, JavaSrc2Cpg}
-import io.joern.x2cpg.X2Cpg.applyDefaultOverlays
-import io.shiftleft.codepropertygraph.generated.Cpg
-import io.shiftleft.codepropertygraph.generated.nodes.NewMynodetype
-import io.shiftleft.passes.CpgPass
-import io.shiftleft.semanticcpg.language._
-import flatgraph.DiffGraphBuilder
-import scala.util.{Failure, Success}
-import io.joern.console.QueryBundle
-import io.joern.console.scan.ScanPass
-import io.joern.console.scan.outputFindings
-
-import com.github.inkspect.java_cli.DangerousFunctions
 import com.github.inkspect.java_cli.infrastructure.cli.{CommandLineOptions, CommandLineConfiguration}
+import com.github.inkspect.java_cli.application.usecases.AnalyzeSourceCodeUseCase
 
 object Main {
-
   def main(args: Array[String]): Unit = {
     CommandLineConfiguration.parse(args) match {
       case Some(options) =>
-        runAnalysis(options.src)
+        AnalyzeSourceCodeUseCase().execute(options.src)
       case None =>
         // arguments are bad, error message will have been displayed
         sys.exit(1)
     }
   }
-
-  private def runAnalysis(src: String): Unit = {
-    println("Hello Joern")
-    println(s"Analyzing: ${src}")
-
-    print("Creating CPG... ")
-    val joernConfig    = Config().withInputPath(src)
-    val cpgOrException = JavaSrc2Cpg().createCpg(joernConfig)
-
-    cpgOrException match {
-      case Success(cpg) =>
-        println("[DONE]")
-        println("Applying default overlays")
-        applyDefaultOverlays(cpg)
-
-        println("Printing all methods:")
-        println("=====================")
-        cpg.method.name.foreach(println)
-        println("=====================")
-
-        // Security analysis
-        println("Running security analysis...")
-        println("Checking for dangerous function calls...")
-
-        val queries = List(DangerousFunctions.execUsed())
-        new ScanPass(cpg, queries).createAndApply()
-        outputFindings(cpg)
-
-      case Failure(exception) =>
-        println("[FAILED]")
-        println(exception)
-        sys.exit(1)
-    }
-  }
-
 }
---
+++ src/main/scala/com/github/inkspect/java_cli/application/usecases/AnalyzeSourceCodeUseCase.scala
@@ -0,0 +1,46 @@
+package com.github.inkspect.java_cli.application.usecases
+
+import io.joern.console.scan.outputFindings
+import io.joern.console.scan.ScanPass
+import io.joern.javasrc2cpg.{Config, JavaSrc2Cpg}
+import io.joern.x2cpg.X2Cpg.applyDefaultOverlays
+import io.shiftleft.semanticcpg.language._
+import scala.util.{Failure, Success}
+
+import com.github.inkspect.java_cli.DangerousFunctions
+
+class AnalyzeSourceCodeUseCase() {
+  def execute(sourcePath: String): Unit = {
+    println("Hello Joern")
+    println(s"Analyzing: ${sourcePath}")
+
+    print("Creating CPG... ")
+    val joernConfig    = Config().withInputPath(sourcePath)
+    val cpgOrException = JavaSrc2Cpg().createCpg(joernConfig)
+
+    cpgOrException match {
+      case Success(cpg) =>
+        println("[DONE]")
+        println("Applying default overlays")
+        applyDefaultOverlays(cpg)
+
+        println("Printing all methods:")
+        println("=====================")
+        cpg.method.name.foreach(println)
+        println("=====================")
+
+        // Security analysis
+        println("Running security analysis...")
+        println("Checking for dangerous function calls...")
+
+        val queries = List(DangerousFunctions.execUsed())
+        new ScanPass(cpg, queries).createAndApply()
+        outputFindings(cpg)
+
+      case Failure(exception) =>
+        println("[FAILED]")
+        println(exception)
+        sys.exit(1)
+    }
+  }
+}
